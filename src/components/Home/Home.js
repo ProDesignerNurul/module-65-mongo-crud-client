02352fdc57fce1be3374f797662ed0f5b47df1f1
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-30 15:14:27 +0600

    added another some data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+
+    const handleDelete = _id => {
+        console.log('deleting id ', _id)
+    }
+
     return (
         <div>
-            <h2> User : </h2>
+            <h2> User : {users.length}</h2>
+            {
+                users.map(user => <p key={user._id}>
+                    {user.name}
+                    {user.email}
+                    <button onClick={ () => handleDelete (user._id)}>x</button>
+                </p>)
+            }
         </div>
     );
 };
